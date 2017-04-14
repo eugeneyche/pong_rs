b7321d530089d9deb35c53eb111adc67725da00d
Eugene Che <eugeneyche@gmail.com>
2017-04-14 10:19:28 -0700

    Paddle collision logic cleanup.

--- src/game.rs
+++ src/game.rs
@@ -11,9 +11,12 @@ const BALL_SPEEDUP: f32 = 1.1;
 const BALL_START_SPEED: f32 = 250.;
 const PADDLE_MAX_SPEED: f32 = 500.;
 const PADDLE_FRICTION: f32 = 0.005;
+const PADDLE_BALL_INFLUENCE: f32 = 0.5;
 const PADDLE_CURVE: f32 = 0.1;
 const PLAYER_PADDLE_ACCEL: f32 = 2000.;
-const AI_PADDLE_ACCEL_FACTOR: f32 = 30.;
+const AI_PADDLE_P_FACTOR: f32 = 120.;
+const AI_PADDLE_I_FACTOR: f32 = 0.5;
+const AI_PADDLE_D_FACTOR: f32 = 10.;
 
 const WIN_SCORE: u32 = 10;
 
@@ -60,11 +63,14 @@ pub struct Board {
     pub rhs_score: u32,
     pub width: f32,
     pub height: f32,
-    pub lhs_paddle: Paddle,
-    pub rhs_paddle: Paddle,
     pub lhs_goal_height: f32,
     pub rhs_goal_height: f32,
+    pub lhs_paddle: Paddle,
+    pub rhs_paddle: Paddle,
     pub ball: Ball,
+    override_ball_sim: bool,
+    ai_last_offset: f32,
+    ai_accum_offset: f32,
 }
 
 fn collides(sx: f32, sy: f32, sdx: f32, sdy: f32, tx: f32, ty: f32, tdx: f32, tdy: f32) -> (f32, f32) {
@@ -73,7 +79,7 @@ fn collides(sx: f32, sy: f32, sdx: f32, sdy: f32, tx: f32, ty: f32, tdx: f32, td
     let nx = (tx + ct * tdx) - sx;
     let ny = (ty + ct * tdy) - sy;
     let cs = (nx * nx + ny * ny) /  (nx * sdx + ny * sdy);
-    (ct, cs)
+    (cs, ct)
 }
 
 impl Board {
@@ -114,7 +120,10 @@ impl Board {
                 },
                 dx: 0.,
                 dy: 0.
-            }
+            },
+            override_ball_sim: false,
+            ai_last_offset: 0.,
+            ai_accum_offset: 0.,
         }
     }
 
@@ -126,7 +135,17 @@ impl Board {
 
     pub fn update(&mut self, dt: f32) {
         // ai sim
-        self.rhs_paddle.ddy = AI_PADDLE_ACCEL_FACTOR * ((self.ball.bound.y + self.ball.bound.height / 2.) - (self.rhs_paddle.bound.y + self.rhs_paddle.bound.height / 2.));
+        let paddle_offset = (self.ball.bound.y + self.ball.bound.height / 2.) - (self.rhs_paddle.bound.y + self.rhs_paddle.bound.height / 2.);
+        if self.ai_accum_offset.signum() != paddle_offset.signum() {
+            self.ai_accum_offset = 0.;
+        } else {
+            self.ai_accum_offset += paddle_offset;
+        }
+        let p = paddle_offset;
+        let i = self.ai_accum_offset;
+        let d = (p - self.ai_last_offset) / dt;
+        self.ai_last_offset = p;
+        self.rhs_paddle.ddy = AI_PADDLE_P_FACTOR * p + AI_PADDLE_I_FACTOR * i + AI_PADDLE_D_FACTOR * d;
         // paddle sim
         for ref mut paddle in [&mut self.lhs_paddle, &mut self.rhs_paddle].iter_mut() {
             paddle.dy += dt * paddle.ddy;
@@ -153,23 +172,26 @@ impl Board {
         let rhs_goal_border_height = (self.height - self.rhs_goal_height) / 2.;
         enum Normal<'a> {
             Static(f32, f32),
-            Dynamic(f32, f32, &'a Fn(f32) -> (f32, f32))
+            Dynamic(f32, f32, &'a Fn(f32, &Ball) -> (f32, f32))
         }
-        fn paddle_sway_normal(is_left: bool, pdy: f32, ct: f32) -> (f32, f32) {
-           let dx = if is_left {1.} else {-1.};
-           let dy = ((2. * ct - 1.) + pdy / PADDLE_MAX_SPEED) * PADDLE_CURVE;
-           let mag = (dx * dx + dy * dy).sqrt();
-           (dx / mag, dy / mag)
+        fn reflect(vx: f32, vy: f32, nx: f32, ny: f32) -> (f32, f32) {
+            let dot = -2. * (nx * vx + ny * vy) / (nx * nx + ny * ny);
+            (vx + dot * nx, vy + dot * ny)
         }
-        let lhs_pdy = self.lhs_paddle.dy;
-        let rhs_pdy = self.rhs_paddle.dy;
-        let lhs_normal_fn = |ct: f32| { paddle_sway_normal(true, lhs_pdy, ct) };
-        let rhs_normal_fn = |ct: f32| { paddle_sway_normal(true, rhs_pdy, ct) };
-        let mut hit_lhs = false;
-        let mut hit_rhs = false;
+        fn paddle_reflect(nx: f32, paddle_dy: f32, ct: f32, ball: &Ball) -> (f32, f32) {
+           let ny = (2. * ct - 1.) * PADDLE_CURVE;
+           let (dx, dy) = reflect(ball.dx, ball.dy, nx, ny);
+           (dx, dy + paddle_dy * PADDLE_BALL_INFLUENCE)
+        }
+        let lhs_paddle_dy = self.lhs_paddle.dy;
+        let rhs_paddle_dy = self.rhs_paddle.dy;
+        let lhs_reflect_fn = |ct: f32, ball: &Ball| { paddle_reflect(1., lhs_paddle_dy, ct, ball) };
+        let rhs_reflect_fn = |ct: f32, ball: &Ball| { paddle_reflect(-1., rhs_paddle_dy, ct, ball) };
+        let mut hit_paddle_lhs = false;
+        let mut hit_paddle_rhs = false;
         {
-            let mut lhs_callback_fn = || { hit_lhs = true; };
-            let mut rhs_callback_fn = || { hit_rhs = true; };
+            let mut lhs_callback_fn = || { hit_paddle_lhs = true; };
+            let mut rhs_callback_fn = || { hit_paddle_rhs = true; };
             let mut basic_colliders: [(f32, f32, f32, f32, Normal, Option<&mut FnMut()>); 8] = [
                 (0., BALL_RADIUS, self.width, 0., Normal::Static(0., 1.), None),
                 (0., self.height - BALL_RADIUS, self.width, 0., Normal::Static(0., -1.), None),
@@ -178,9 +200,9 @@ impl Board {
                 (self.width - BALL_RADIUS, 0., 0., lhs_goal_border_height, Normal::Static(-1., 0.), None),
                 (self.width - BALL_RADIUS, self.height, 0., -rhs_goal_border_height, Normal::Static(-1., 0.), None),
                 (self.lhs_paddle.bound.x + self.lhs_paddle.bound.width + BALL_RADIUS, self.lhs_paddle.bound.y, 0., self.lhs_paddle.bound.height, 
-                    Normal::Dynamic(1., 0., &lhs_normal_fn), Some(&mut lhs_callback_fn)),
+                    Normal::Dynamic(1., 0., &lhs_reflect_fn), Some(&mut lhs_callback_fn)),
                 (self.rhs_paddle.bound.x - BALL_RADIUS, self.rhs_paddle.bound.y, 0., self.rhs_paddle.bound.height, 
-                    Normal::Dynamic(-1., 0., &rhs_normal_fn), Some(&mut rhs_callback_fn)),
+                    Normal::Dynamic(-1., 0., &rhs_reflect_fn), Some(&mut rhs_callback_fn)),
             ];
             while dt_left > 0. && has_collide && iterations < MAX_ITERATIONS {
                 iterations += 1;
@@ -188,38 +210,39 @@ impl Board {
                 let iter_dx = self.ball.dx * dt_left;
                 let iter_dy = self.ball.dy * dt_left;
                 for &mut (tx, ty, tdx, tdy, ref normal, ref mut callback) in basic_colliders.iter_mut() {
-                    let (mut nx, mut ny) = match normal {
+                    let (nx, ny) = match normal {
                         &Normal::Static(x, y) => (x, y),
                         &Normal::Dynamic(x, y, _) => (x, y)
                     };
                     if nx * self.ball.dx + ny * self.ball.dy >= 0. { continue; }
                     let (cs, ct) = collides(self.ball.bound.x + BALL_RADIUS, self.ball.bound.y + BALL_RADIUS, iter_dx, iter_dy, tx, ty, tdx, tdy);
                     if 0. < cs && cs <= 1. && 0. < ct && ct <= 1. {
-                        if let &Normal::Dynamic(_, _, f) = normal {
-                            let (nnx, nny) = f(ct);
-                            nx = nnx;
-                            ny = nny;
-                        }
                         has_collide = true;
                         self.ball.bound.x += iter_dx * (cs - 0.001);
                         self.ball.bound.y += iter_dy * (cs - 0.001);
-                        let dot = -2. * (nx * self.ball.dx + ny * self.ball.dy);
-                        self.ball.dx += dot * nx;
-                        self.ball.dy += dot * ny;
+                        let (dx, dy) = if let &Normal::Dynamic(_, _, reflect_fn) = normal {
+                            reflect_fn(ct, &self.ball)
+                        } else {
+                            reflect(self.ball.dx, self.ball.dy, nx, ny)
+                        };
+                        self.ball.dx = dx;
+                        self.ball.dy = dy;
                         dt_left *= 1. - cs;
-                        if let &mut Some(ref mut f) = callback {
-                            f();
+                        if let &mut Some(ref mut callback_fn) = callback {
+                            callback_fn();
                         }
                         break;
                     }
                 }
             }
         }
-        if hit_lhs || hit_rhs {
-            self.ball.speedup(BALL_SPEEDUP);
+        if !self.override_ball_sim {
+            if hit_paddle_lhs || hit_paddle_rhs {
+                self.ball.speedup(BALL_SPEEDUP);
+            }
+            self.ball.bound.x += self.ball.dx * dt_left;
+            self.ball.bound.y += self.ball.dy * dt_left;
         }
-        self.ball.bound.x += self.ball.dx * dt_left;
-        self.ball.bound.y += self.ball.dy * dt_left;
         if self.ball.bound.x < 0. {
             self.rhs_score += 1;
             self.start_game(true);
@@ -240,8 +263,10 @@ impl Board {
         self.rhs_paddle.ddy = 0.;
         self.ball.bound.x = BOARD_WIDTH / 2. - BALL_RADIUS;
         self.ball.bound.y = BOARD_HEIGHT / 2. - BALL_RADIUS;
-        self.ball.dy = 0.;
         self.ball.dx = if lhs_start { -BALL_START_SPEED } else { BALL_START_SPEED };
+        self.ball.dy = 0.;
+        self.ai_last_offset = 0.;
+        self.ai_accum_offset = 0.;
     }
 
     pub fn handle_input(&mut self, key: glium::glutin::VirtualKeyCode, is_pressed: bool) {
@@ -262,6 +287,22 @@ impl Board {
                 if self.lhs_paddle.ddy < 0. {
                     self.lhs_paddle.ddy = 0.;
                 }
+            },
+            (glium::glutin::VirtualKeyCode::B, true) => {
+                self.override_ball_sim = !self.override_ball_sim;
+            },
+            (glium::glutin::VirtualKeyCode::W, true) => {
+                if self.override_ball_sim {
+                    self.ball.bound.y += 100.;
+                }
+            },
+            (glium::glutin::VirtualKeyCode::S, true) => {
+                if self.override_ball_sim {
+                    self.ball.bound.y -= 100.;
+                }
+            },
+            (glium::glutin::VirtualKeyCode::Return, true) => {
+                self.start_game(true);
             },
             _ => ()
         }
